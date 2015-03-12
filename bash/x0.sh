1a7535ba90932949e9d9c7d4ff0835f1b744dd2b
furgoo <ferdinand.go@gmail.com>
2015-03-13 00:40:21 +0800

    modified x0.sh

--- bash/x0.sh
+++ bash/x0.sh
@@ -10,5 +10,28 @@ yum install gcc make rpm-build autoconf.noarch zlib-devel pam-devel openssl-deve
 wget -c http://openvpn.net/release/lzo-1.08-4.rf.src.rpm && wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm && rpmbuild --rebuild lzo-1.08-4.rf.src.rpm && rpm -Uvh lzo-*.rpm && rpm -Uvh rpmforge-release* && yum install openvpn 
 #
 cp -R /usr/share/doc/openvpn-2.2.2/easy-rsa/ /etc/openvpn/
-[root@openvpn ~]# vi /etc/openvpn/easy-rsa/2.0/vars 
+#
+wget -c https://raw.githubusercontent.com/furgoo/Scripts/master/bash/vars
+#
+cp vars /etc/openvpn/easy-rsa/2.0/vars 
+#
+cd /etc/openvpn/easy-rsa/2.0 && chmod 755 * && source ./vars && ./build-ca
+#
+./clean-all && ./build-ca
+#
+wget -c https://github.com/furgoo/Scripts/blob/master/bash/etc.openvpn.tbz2
+#
+tar xvjf etc.openvpn.tbz2
+#
+cp /etc/ /etc.old/ -rvf
+#
+cp etc.openvpn/* /etc/ -rvf
+#
+#
+#       ./build-dh
+
+
+
+
+
 
